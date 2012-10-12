994a2448b1ba0478983c185feb450a0a28011a9f
Michael Gauthier <mike@silverorange.com>
2012-10-12 16:18:32 +0000

    Docs

--- Net/ChaChing/WebSocket/ClientException.php
+++ Net/ChaChing/WebSocket/ClientException.php
@@ -1,5 +1,50 @@
 <?php
 
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Client exception class definition
+ *
+ * PHP version 5
+ *
+ * LICENSE:
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @category  Net
+ * @package   ChaChing
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2012 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+
+/**
+ * Base exception interface
+ */
+require_once 'Net/ChaChing/Exception.php';
+
+/**
+ * Exception thrown when a WebSocket client fails to connect due to bad
+ * client parameters
+ *
+ * @category  Net
+ * @package   Net_ChaChing
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2012 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
 class Net_ChaChing_WebSocket_ClientException
     extends Exception
     implements Net_ChaChing_Exception

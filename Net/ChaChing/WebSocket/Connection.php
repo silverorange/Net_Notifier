da0bc545913fb67541ea940217c59fd0271975b0
Michael Gauthier <mike@silverorange.com>
2012-10-14 22:02:56 +0000

    Add docs

--- Net/ChaChing/WebSocket/Connection.php
+++ Net/ChaChing/WebSocket/Connection.php
@@ -3,6 +3,34 @@
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
+ * Cha-ching WebSocket connection class
+ *
+ * PHP version 5
+ *
+ * LICENSE:
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @category  Net
+ * @package   ChaChing
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2011-2012 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
+
+/**
  * ChaChing WebSocket protocol definition.
  */
 require_once 'Net/ChaChing/WebSocket.php';
@@ -37,6 +65,18 @@ require_once 'Net/ChaChing/WebSocket/Frame.php';
  */
 require_once 'Net/ChaChing/WebSocket/FrameParser.php';
 
+/**
+ * A WebSocket connection
+ *
+ * Handles hansdhaking, sending and receiving WebSocket messages and control
+ * frames.
+ *
+ * @category  Net
+ * @package   ChaChing
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2011-2012 silverorange
+ * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
+ */
 class Net_ChaChing_WebSocket_Connection
 {
     // {{{ class constants
